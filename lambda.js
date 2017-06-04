f859ab2f025688d98bd584ffa89e0c5816be6b1d
Joshua Sandlin <josh.sandlin@turner.com>
2017-06-04 07:43:43 -0400

    man this is going to be weird

--- lambda.js
+++ lambda.js
@@ -4,4 +4,4 @@ const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./src/app');
 const server = awsServerlessExpress.createServer(app);
 
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
+module.exports.main = (event, context) => awsServerlessExpress.proxy(server, event, context);
